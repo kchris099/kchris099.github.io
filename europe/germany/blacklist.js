1ee3a6832acd85a51fda7f6bb5b81aaf69391c46
kchris099 <chrisfoerster99@gmail.com>
2026-06-21 12:59:43 -0700

    website update 20260621 v2
    Inactive guides can't be clicked
    Active guides show up first in search and listing
    Germany has blacklist for inactive guides
    Japan, Turkey Prices update
    Destination count in the index map

---
+++ europe/germany/blacklist.js
@@ -0,0 +1,61 @@
+// To disable a guide, add the destination name on a new line below.
+// You do not need to write code. Just type the city name normally between the backticks.
+
+const blacklistText = `
+# All German destinations EXCEPT:
+# Western Germany (Aachen, Bonn, Brühl, Cologne, Düsseldorf, Frankfurt, Monschau, Moselle Valley, Rhine Valley, Trier)
+# Dresden
+# Meissen
+# Sächsische Schweiz National Park
+# Heidelberg
+
+# Central Germany
+Eisenach
+Erfurt
+Goslar
+Harz Mountains
+Kassel
+Mühlhausen
+Quedlinburg
+Weimar
+
+# Eastern Germany
+Berlin
+Leipzig
+Moritzburg
+
+# Northern Germany
+Bremen
+Celle
+Greifswald
+Göttingen
+Hamburg
+Hameln
+Hannover
+Lübeck
+Stralsund
+Wismar
+Wolfsburg
+
+# Southern Germany
+Augsburg
+Baden-Baden
+Bamberg
+Black Forest
+Dinkelsbühl
+Esslingen
+Freiburg
+Füssen
+Garmisch-Partenkirchen
+Lake Constance
+Lindau
+Ludwigsburg
+Munich
+Nuremberg
+Nördlingen
+Rothenburg ob der Tauber
+Stuttgart
+Tübingen
+Ulm
+Würzburg
+`;
